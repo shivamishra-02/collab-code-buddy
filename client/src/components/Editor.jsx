41d6b2d4e09fc48956ab7a8e372951a233b994dd
Shivam mishra <shivammishra993187@gmail.com>
2026-06-12 22:43:14 +0530

    Editor Component

---
+++ client/src/components/Editor.jsx
@@ -0,0 +1,25 @@
+import MonacoEditor from "@monaco-editor/react";
+
+function Editor({ code, language, onChange }) {
+  const handleEditorChange = (value) => {
+    onChange(value);
+  };
+
+  return (
+    <MonacoEditor
+      height="100%"
+      language={language}
+      theme="vs-dark"
+      value={code}
+      onChange={handleEditorChange}
+      options={{
+        fontSize: 16,
+        minimap: { enabled: false },
+        wordWrap: "on",
+        automaticLayout: true,
+      }}
+    />
+  );
+}
+
+export default Editor;
